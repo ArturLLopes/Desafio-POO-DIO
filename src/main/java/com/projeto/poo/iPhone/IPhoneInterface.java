71610f6f5191823a01d34d471b38fdd1d263a62b
ArturLLopes <arturllopes@gmail.com>
2025-05-26 10:42:04 -0300

    Feat: add interface para controle da classe

---
+++ src/main/java/com/projeto/poo/iPhone/IPhoneInterface.java
@@ -0,0 +1,21 @@
+package com.projeto.poo.iPhone;
+
+public interface IPhoneInterface {
+    public interface ReprodutorMusical {
+        void tocar();
+        void pausar();
+        void selecionarMusica(String musica);
+    }
+
+    public interface AparelhoTelefonico {
+        void ligar(String numero);
+        void atender();
+        void iniciarCorreioVoz();
+    }
+
+    public interface NavegadorInternet {
+        void exibirPagina(String url);
+        void adicionarNovaAba();
+        void atualizarPagina();
+    }
+}
